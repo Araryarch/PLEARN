a40989331ee847c1b9a5490b91f3c60719089680
Araryarch <araryaarch@gmail.com>
2025-10-31 00:44:45 +0700

    fix: fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,6 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli'
 const config: CapacitorConfig = {
   appId: 'com.plearn.app',
   appName: 'plearn',
+  webDir: '.next',
   server: {
     url: 'https://plearn-zeta.vercel.app/',
     cleartext: true,
