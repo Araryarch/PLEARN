3595eecea8916a31b263931cf4f14c78240da0a2
Araryarch <araryaarch@gmail.com>
2025-10-31 15:59:30 +0700

    fix: fix

--- src/app/api/todo/[id]/route.ts
+++ src/app/api/todo/[id]/route.ts
@@ -1,13 +1,14 @@
 import { NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
 
-export async function PUT(
-  req: Request,
-  { params }: { params: { id: string } },
-) {
+interface Context {
+  params: { id: string }
+}
+
+export async function PUT(req: Request, context: Context) {
   try {
     const body = await req.json()
-    const id = parseInt(params.id)
+    const id = parseInt(context.params.id)
     const updated = await prisma.todo.update({
       where: { id },
       data: body,
@@ -15,20 +16,17 @@ export async function PUT(
     return NextResponse.json(updated)
   } catch (err) {
     console.error(err)
-    return NextResponse.json({ error: 'Gagal update tugas' }, { status: 400 })
+    return NextResponse.json({ error: 'gagal update tugas' }, { status: 400 })
   }
 }
 
-export async function DELETE(
-  req: Request,
-  { params }: { params: { id: string } },
-) {
+export async function DELETE(req: Request, context: Context) {
   try {
-    const id = parseInt(params.id)
+    const id = parseInt(context.params.id)
     await prisma.todo.delete({ where: { id } })
     return NextResponse.json({ success: true })
   } catch (err) {
     console.error(err)
-    return NextResponse.json({ error: 'Gagal hapus tugas' }, { status: 400 })
+    return NextResponse.json({ error: 'gagal hapus tugas' }, { status: 400 })
   }
 }
