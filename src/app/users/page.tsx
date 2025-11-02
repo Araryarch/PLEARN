7c5e0b245ec42a4e74b7541d9dd2eba186336089
Araryarch <araryaarch@gmail.com>
2025-11-03 03:56:33 +0700

    fix: fix

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -128,6 +128,8 @@ export default function Page() {
 
   const extended = session as ExtendedSession
 
+  console.log(extended.user.avatar)
+
   return (
     <Layouts>
       <div className="min-h-screen w-full bg-[#1e1e2e] text-[#cdd6f4] flex flex-col gap-4 p-4 pb-16">
